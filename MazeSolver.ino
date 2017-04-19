6e4bcf6129dd0dba019358dc1bb94c45493fb1ae
wang-dw <wangdw@miamioh.edu>
2017-04-19 18:46:02 -0400

    Prototype #2
    Added 2nd ultrasonic sensor + Code

--- MazeSolver.ino
+++ MazeSolver.ino
@@ -1,81 +1,99 @@
-//left
-const int trigPin = 4;
-const int echoPin = 2;
-//front
-const int trigPin1 = 5;
-const int echoPin1 = 3;
+/*Maze solving robot using the left hand rule
+  Written By: David Wang
+              Dai Hong Zheng
+              Ben Wong
+*/
+
+const int trigPinLeft = 3;
+const int echoPinLeft = 2;
+
+const int trigPinFront = 5;
+const int echoPinFront = 4;
+
+long durationLeft;
+long durationFront;
 
 void setup()
 {
+
   Serial.begin(9600);
+
+  //LEFT
   pinMode(7, OUTPUT);
   pinMode(8, OUTPUT);
   pinMode(9, OUTPUT);
 
+  //RIGHT
   pinMode(13, OUTPUT);
   pinMode(12, OUTPUT);
   pinMode(11, OUTPUT);
 
-  
+  pinMode(echoPinLeft, INPUT);
+  pinMode(echoPinFront, INPUT);
+
+  pinMode(trigPinLeft, OUTPUT);
+  pinMode(trigPinFront, OUTPUT);
+
 }
 
 void loop()
 {
-  long durationLeft, inches, cm;
-  //left
-  pinMode(trigPin, OUTPUT);
-  digitalWrite(trigPin, LOW);
+  digitalWrite(trigPinLeft, LOW);
   delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
+  digitalWrite(trigPinLeft, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-   //front
-  pinMode(trigPin1, OUTPUT);
-  digitalWrite(trigPin1, LOW);
+  digitalWrite(trigPinLeft, LOW);
+
+  durationLeft = pulseIn(echoPinLeft, HIGH);
+
+  digitalWrite(trigPinFront, LOW);
   delayMicroseconds(2);
-  digitalWrite(trigPin1, HIGH);
+  digitalWrite(trigPinFront, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trigPin1, LOW);
+  digitalWrite(trigPinFront, LOW);
 
-  pinMode(echoPin, INPUT);
-  durationLeft = pulseIn(echoPin, HIGH);
 
-  inches = microsecondsToInches(durationLeft);
-  cm = microsecondsToCentimeters(durationLeft);
+  durationFront = pulseIn(echoPinFront, HIGH);
 
-  Serial.print(inches);
-  Serial.print("in, ");
-  Serial.print(cm);
-  Serial.print("cm");
-  Serial.println();
-  if(durationLeft < 300)
+  sensor();
+  Serial.print("Left: ");
+  Serial.println(durationLeft);
+  Serial.print("Front: ");
+  Serial.println(durationFront);
+
+  delay(500);
+}
+
+void sensor()
+{
+  if (durationFront > 650) //IF NO WALL IN FRONT
   {
-    //LEFT
+    if (durationLeft < 300) //TURN SLIGHT RIGHT IF TOO CLOSE
+    {
+      //LEFT
       digitalWrite(8, LOW);
       digitalWrite(7, HIGH);
-      digitalWrite(9, 50 );  
+      digitalWrite(9, 50 );
 
       //RIGHT
       digitalWrite(12, LOW);
       digitalWrite(13, HIGH);
       digitalWrite(11, 0);
-  }
-  else
-  {
-    if(durationLeft < 650)
-   {
-    //LEFT
+    }
+    else if (durationLeft < 650) //GO STRAIGHT
+    {
+      //LEFT
       digitalWrite(8, LOW);
       digitalWrite(7, HIGH);
-      digitalWrite(9, 200 );  
+      digitalWrite(9, 200 );
 
       //RIGHT
       digitalWrite(12, LOW);
       digitalWrite(13, HIGH);
       digitalWrite(11, 60);
-   }
-   else if(durationLeft >= 650)
-   {
+    }
+    else //if (durationLeft < 750) //TURN SLIGHT LEFT IF TOO FAR
+    {
       //LEFT Wheel
       digitalWrite(8, LOW);
       digitalWrite(7, LOW);
@@ -86,26 +104,35 @@ void loop()
       digitalWrite(13, HIGH);
       digitalWrite(11, 60);
 
-   }
+    }
   }
-   delay(100);
-}
+  else //IS WALL
+  {
+    if (durationLeft > 750) //IF NO LEFT WALL
+    {
+      //LEFT Wheel
+      digitalWrite(8, LOW);
+      digitalWrite(7, LOW);
+      digitalWrite(9, 90);
 
-long microsecondsToInches(long microseconds) 
-{
-  // According to Parallax's datasheet for the PING))), there are
-  // 73.746 microseconds per inch (i.e. sound travels at 1130 feet per
-  // second).  This gives the distance travelled by the ping, outbound
-  // and return, so we divide by 2 to get the distance of the obstacle.
-  // See: http://www.parallax.com/dl/docs/prod/acc/28015-PING-v1.3.pdf
-  return microseconds / 74 / 2;
-}
+      //RIGHT Wheel
+      digitalWrite(12, LOW);
+      digitalWrite(13, HIGH);
+      digitalWrite(11, 60);
+    }
+    else
+    {
+      //TURN RIGHT
+      //LEFT Wheel
+      digitalWrite(8, LOW);
+      digitalWrite(7, LOW);
+      digitalWrite(9, 90);
 
-long microsecondsToCentimeters(long microseconds) 
-{
-  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
-  // The ping travels out and back, so to find the distance of the
-  // object we take half of the distance travelled.
-  return microseconds / 29 / 2;
+      //RIGHT Wheel
+      digitalWrite(12, LOW);
+      digitalWrite(13, HIGH);
+      digitalWrite(11, 0);
+    }
+  }
 }
 
